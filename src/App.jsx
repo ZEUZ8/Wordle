198e00e1d68d07aa341a37edeb09589e809294f6
sinan <ptsinan8590@gmail.com>
2023-11-09 10:33:39 +0530

    Integrate JSON Server and Implement Random Letter Selection
    Utilized JSON Server to create a data source, fetching word data to use in the game. Implemented logic to select a random letter from the retrieved data for use in the game.

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,25 @@
 import { useState, useEffect } from "react";
+import cors from "cors"
 import "./App.css";
-
 function App() {
-  const [word, setWord] = useState("");
+  const [solution,setSolutions] = useState(null)
+  // const [word, setWord] = useState("");
 
   useEffect(() => {
-    fetch("https://wordsapiv1.p.rapidapi.com/words/?letters=5", {
-      method: "GET",
-      headers: {
-        "X-RapidAPI-Host": "wordsapiv1.p.rapidapi.com",
-        "X-RapidAPI-Key": "a8e96a89efmshc1435e061e16e4fp148869jsnd2de258343b9",
-      },
+    fetch("http://localhost:3000/solutions")
+    .then(res => res.json())
+    .then(json =>{
+      const randomSolution = json[Math.floor(Math.random()*json.length)]
+      setSolutions(randomSolution)
+      console.log(randomSolution)
     })
-      .then((response) => response.json())
-      .then((data) => {
-        console.log('in the response section')
-        console.log(data,'  the dat hae hase')
-        const fiveLetterWords = data.words;
-        console.log(fiveLetterWords,'  whte words in the input')
-      })
-      .catch((error) => {
-        console.error("Error fetching words:", error);
-      });
-  }, [1]);
+  }, [setSolutions]);
 
   return (
     <>
       <h1>  Wordle App</h1>
       <p>Guess the five-letter word:</p>
-      <p>{word}</p>
-      {/* Add your game logic and UI components here */}
+      {solution && <div>the solution is : {solution.word}</div>}
     </>
   );
 }
